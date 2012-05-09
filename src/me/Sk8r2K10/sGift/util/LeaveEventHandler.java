3e7d01048ab02b754a5f08926afd8558436c431e
Sk8r2K10 <Sk8r2k9@hotmail.co.uk>
2012-05-09 10:51:53 +0100

    Continued Leave Event handler. Added config node to use LeaveEvent.

--- src/me/Sk8r2K10/sGift/util/LeaveEventHandler.java
+++ src/me/Sk8r2K10/sGift/util/LeaveEventHandler.java
@@ -4,6 +4,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import me.Sk8r2K10.sGift.sGift;
 import net.milkbowl.vault.item.Items;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,89 +14,138 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class LeaveEventHandler implements Listener{
-	
-	private sGift plugin = new sGift();
-	private Player player;
-	private ItemStack item;
-	private ItemStack vItem;
-	private int amount;
-	private ResultSet result;
-	
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerLeave (PlayerQuitEvent e) {
-		try {
-			result = plugin.SQL.scanSender(e.getPlayer());
-			
-			player = plugin.getServer().getPlayer(result.getString("player"));
-			if (result.getString("player").equals(e.getPlayer().getName())) {
-				result.close();
-				
-				result = plugin.SQL.scanGift(player);
-				
-				if (result.getString("player").equals(player.getName())) {
-					
-					item = Items.itemByName(result.getString("Item")).toStack();
-					amount = result.getInt("amount");
-					
-					result.close();
-					
-					item.setAmount(amount);
-					
-					plugin.SQL.addLost(player, item, amount);
-				} else {
-					result.close();
-				}
-				
-				
-				result = plugin.SQL.scanSwap(player);
-				
-				if (result.getString("player").equals(player.getName())) {
-					
-					item = Items.itemByName(result.getString("Item")).toStack();
-					amount = result.getInt("amount");
-					
-					result.close();
-					
-					item.setAmount(amount);
-					
-					plugin.SQL.addLost(player, item, amount);
-					
-				} else if (result.getString("Victim").equals(player.getName())) {
-					
-					item = Items.itemByName(result.getString("ItemFromVictim")).toStack();
-					amount = result.getInt("amountFromVictim");
-					
-					result.close();
-					
-					item.setAmount(amount);
-					
-					plugin.SQL.addLost(player, item, amount);					
-				} else {
-					result.close();
-				}
-				
-				result = plugin.SQL.scanTrade(player);
-				
-				if (result.getString("player").equals(player.getName())) {
-					
-					item = Items.itemByName(result.getString("Item")).toStack();
-					amount = result.getInt("amount");
-					
-					result.close();
-					
-					item.setAmount(amount);
-					
-					plugin.SQL.addLost(player, item, amount);
-					
-				} else {
-					result.close();
-				}
-				
-				// Add to "Left during transaction" table
-			}
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-		}
-	}
+public class LeaveEventHandler implements Listener {
+
+    private sGift plugin = new sGift();
+    private Player player;
+    private Player Victim;
+    private ItemStack item;
+    private ItemStack vItem;
+    private int amount;
+    private int vAmount;
+    private int price;
+    private ResultSet result;
+    private String errpre = "[" + ChatColor.RED + "sGift" + ChatColor.WHITE + "] " + ChatColor.RED;
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerLeave(PlayerQuitEvent e) {
+        if (!plugin.getConfig().getBoolean("Features.cancel-exchanges-on-leave")) {
+            
+            return;
+        }
+        if (!plugin.getConfig().getBoolean("Options.use-sql.sqlite") && !plugin.getConfig().getBoolean("Options.use-sql.mysql.use")) {
+            
+            return;
+        }
+        
+        try {
+            result = plugin.SQL.scanSender(e.getPlayer());
+
+            player = plugin.getServer().getPlayer(result.getString("player"));
+            if (result.getString("player").equals(e.getPlayer().getName())) {
+                result.close();
+
+                result = plugin.SQL.scanGift(player);
+
+                if (result.getString("player").equals(player.getName())) {
+                    
+                    Victim = Bukkit.getPlayer(result.getString("Victim"));
+                    item = Items.itemByName(result.getString("Item")).toStack();
+                    amount = result.getInt("amount");
+
+                    result.close();
+
+                    item.setAmount(amount);
+                    
+                    Victim.sendMessage(errpre + "Gift ended, Player sending items left the game.");
+
+                    plugin.SQL.addLost(player, item, amount);
+                    plugin.SQL.removeGift(player, Victim, item, amount);
+                } else {
+                    result.close();
+                }
+
+                result = plugin.SQL.scanSwap(player);
+
+                if (result.getString("player").equals(player.getName())) {
+                    
+                    Victim = Bukkit.getPlayer(result.getString("Victim"));
+                    item = Items.itemByName(result.getString("Item")).toStack();
+                    vItem = Items.itemByName(result.getString("ItemFromVictim")).toStack();
+                    amount = result.getInt("amount");
+                    vAmount = result.getInt("amountFromVictim");
+                    
+                    result.close();
+
+                    item.setAmount(amount);
+                    vItem.setAmount(vAmount);
+                    
+                    this.stopSwap(Victim, vItem);
+
+                    plugin.SQL.addLost(player, item, amount);
+                    plugin.SQL.removeSwap(player, Victim, item, amount, vItem, vAmount);
+                } else if (result.getString("Victim").equals(player.getName())) {
+
+                    Victim = Bukkit.getPlayer(result.getString("Victim"));
+                    item = Items.itemByName(result.getString("Item")).toStack();
+                    vItem = Items.itemByName(result.getString("ItemFromVictim")).toStack();
+                    amount = result.getInt("amount");
+                    vAmount = result.getInt("amountFromVictim");
+
+                    result.close();
+
+                    item.setAmount(amount);
+                    vItem.setAmount(vAmount);
+                    
+                    this.stopSwap(player, item);
+                    
+                    plugin.SQL.addLost(player, vItem, vAmount);
+                    plugin.SQL.removeSwap(player, Victim, item, amount, vItem, vAmount);
+                } else {
+                    result.close();
+                }
+
+                result = plugin.SQL.scanTrade(player);
+
+                if (result.getString("player").equals(player.getName())) {
+                    
+                    Victim = Bukkit.getPlayer(result.getString("Victim"));
+                    item = Items.itemByName(result.getString("Item")).toStack();
+                    amount = result.getInt("amount");
+                    price = result.getInt("price");
+
+                    result.close();
+
+                    item.setAmount(amount);
+                    
+                    Victim.sendMessage(errpre + "Trade ended, Player offering items left the game.");
+
+                    plugin.SQL.addLost(player, item, amount);
+                    plugin.SQL.removeTrade(player, Victim, item, amount, price);
+
+                } else {
+                    result.close();
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void stopSwap(Player Victim, ItemStack vItem) {
+        
+        Victim.sendMessage(errpre + "Other player left the swap, Returning your items.");
+        
+        if (Victim.getInventory().firstEmpty() == -1) {
+            
+            Location VicLoc = Victim.getLocation();
+            
+            VicLoc.getWorld().dropItemNaturally(VicLoc, vItem);
+            Victim.sendMessage(errpre + "No room in your inventory!");
+            Victim.sendMessage(errpre + "Dropped items at your feet.");
+        } else {
+            
+            Victim.getInventory().addItem(vItem);
+        }
+    }
 }
